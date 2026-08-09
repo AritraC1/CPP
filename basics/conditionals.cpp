c616d2baa4dc0bcc240884d7b641df727912448e
Aritra Chatterjee <ach142812@gmail.com>
2026-08-10 00:00:14 +0530

    added conditionals

---
+++ basics/conditionals.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+// conditionals
+void isAdult(int age) {
+    if (age < 18) {
+        cout << "Not an Adult" << endl;
+    }
+
+    else if (age == 18) {
+        cout << "Just Adult" << endl;
+    }
+
+    else {
+        cout <<  "Yes Adult" << endl;
+    }
+}
--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 // #include "basics/variables.cpp"
+#include "basics/conditionals.cpp"
 using namespace std;
 
 int main() {
     cout << "Hello World" << endl;
     
     // variables();
+    isAdult(12);
 
     return 0;
 }
