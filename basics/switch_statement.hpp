929400ade8d34e00e36751a40d00e1c049455a54
Aritra Chatterjee <ach142812@gmail.com>
2026-08-10 13:30:07 +0530

    Added switch statement

---
+++ basics/switch_statement.hpp
@@ -0,0 +1,3 @@
+#pragma once
+
+void dayDate(int num);
--- main.cpp
+++ main.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
-#include "basics/conditionals.hpp"
+#include "basics/switch_statement.hpp"
 using namespace std;
 
 int main() {
     cout << "Hello World" << endl;
     
     // variables();
-    isAdult(12);
+    // isAdult(12);
+    dayDate(8);
 
     return 0;
 }
